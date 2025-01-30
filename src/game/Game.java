f60dff8caf909fda906f9261c6cb4b3161d633c8
hermansildnes <herman.sildnes@city.ac.uk>
2025-01-30 13:22:13 +0000

    Added custom character + played around with world building

--- src/game/Game.java
+++ src/game/Game.java
@@ -8,6 +8,9 @@ import javax.swing.JFrame;
 
 import java.awt.*;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
@@ -34,13 +37,38 @@ public class Game {
         Shape platformShape = new BoxShape(3, 0.5f);
         StaticBody platform1 = new StaticBody(world, platformShape);
         platform1.setPosition(new Vec2(-8, -4f));
+        platform1.rotateDegrees(-45f);
+
+        StaticBody platform2 = new StaticBody(world, platformShape);
+        platform2.setPosition(new Vec2(8, 5.5f));
+
+        Shape wallShape = new BoxShape(0.5f, 10f);
+        StaticBody wall1 = new StaticBody(world, wallShape);
+        StaticBody wall2 = new StaticBody(world, wallShape);
+        wall1.putOn(-30f, ground);
+        wall2.putOn(30f, ground);
+
+
+        Shape ballShape = new CircleShape(1.5f);
+        DynamicBody ball = new DynamicBody(world, ballShape);
+        //ball.setPosition(new Vec2(-8f, -2f));
+        ball.putOn(platform1);
 
         //make a character (with an overlaid image)
-        Shape studentShape = new BoxShape(1,2);
-        DynamicBody student = new DynamicBody(world, studentShape);
-        student.setPosition(new Vec2(4,-5));
-        student.addImage(new BodyImage("data/student.png", 4));
+        //Shape studentShape = new BoxShape(1,2);
+      //  DynamicBody student = new DynamicBody(world, studentShape);
+    //    student.setPosition(new Vec2(4,-5));
+  //      student.addImage(new BodyImage("data/student.png", 4));
+//
 
+        // CharacterHitbox:
+        // -0.8f,-1.98f, -0.86f,-0.81f, -1.32f,-0.78f, -1.34f,0.66f, -0.8f,1.21f, -0.66f,1.99f, 0.54f,1.99f, 0.66f,1.2f, 1.19f,0.68f, 1.19f,-0.78f, 0.72f,-0.78f, 0.66f,-1.97f
+        //Shape characterShape = new BoxShape(1f, 2f);
+
+        Shape characterShape = new PolygonShape(0.66f,-1.97f, 1.2f,-0.78f, 1.19f,0.86f, 0.4f,1.99f, -0.54f,1.98f, -1.33f,0.9f, -1.34f,-0.78f, -0.79f,-1.98f);
+        DynamicBody character = new DynamicBody(world, characterShape);
+        character.setPosition(new Vec2(4,-5));
+        character.addImage(new BodyImage("data/character.png", 4));
 
         //3. make a view to look into the game world
         UserView view = new UserView(world, 500, 500);
